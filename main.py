a3cf8962fe692b7fdcb77e8959f1c6d5c4cab842
MartinMyska <martin.mysska@gmail.com>
2025-04-23 14:49:04 +0200

    main: Add func to start and reset button

--- main.py
+++ main.py
@@ -1,19 +1,58 @@
 import tkinter as tk
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 1
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    title_label.config(text="Timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# --------------------------- TIMER MECHANISM ---------------------------- #
+
+
+def start_timer():
+    global reps
+    reps += 1
+    if reps % 2 != 0:
+        count_down(WORK_MIN * 60)
+        title_label.config(text="Work", fg=GREEN)
+    elif reps % 8 == 0:
+        count_down(LONG_BREAK_MIN * 60)
+        title_label.config(text="Break", fg=RED)
+    else:
+        count_down(SHORT_BREAK_MIN * 60)
+        title_label.config(text="Break", fg=RED)
+
+
+# ----------------------- COUNTDOWN MECHANISM -------------------------- #
+
+def count_down(count):
+    minutes = count // 60
+    seconds = count - minutes * 60
+    if seconds < 10:
+        seconds = f"0{seconds}"
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        check_marks.config(text=math.floor(reps/2) * "✓")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -28,16 +67,16 @@ title_label.grid(column=1, row=0)
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = tk.Button(text="Start", highlightthickness=0)
+start_button = tk.Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = tk.Button(text="Reset", highlightthickness=0, fg=GREEN)
+reset_button = tk.Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_marks = tk.Label(text="✓", fg=GREEN, bg=YELLOW)
+check_marks = tk.Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 window.mainloop()
